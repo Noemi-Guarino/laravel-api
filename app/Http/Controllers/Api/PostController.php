db1e10a349ab9269a5555747fc7c0df045e24578
Noemi-Guarino <noemi.guarino.1996@gmail.com>
2024-03-13 14:33:36 +0100

    creat a post controller

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+// Models
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::with('type', 'technologies')->paginate(6);
+
+        return response()->json([
+            'success' => true,
+            'results' => $posts,
+        ]);
+    }
+
+    public function show(string $slug)
+    {
+        $post = Post::where('slug', $slug)->firstOrFail();
+
+        return response()->json([
+            'success'=> true,
+            'results'=> $post,
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// Controllers
+use App\Http\Controllers\Api\PostController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,7 +16,19 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::name('api.')->group(function() {
+    Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+        return $request->user();
+    });
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    // Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+    // Route::get('/posts', [PostController::class, 'show'])->name('posts.show');
+
+    // OPPURE
+
+    //con il resourse noi chiamavamo le 7 rotte delle crud con la funzionalità only gli passo le uniche due rotte che deve utilizzare
+    Route::resource('posts', PostController::class)->only([
+        'index',
+        'show'
+    ]);
 });
