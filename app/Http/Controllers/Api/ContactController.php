01f0517155d9c646065d280f697bea264d66de8d
Noemi-Guarino <noemi.guarino.1996@gmail.com>
2024-03-15 17:19:44 +0100

    migration,model,of contact

--- app/Http/Controllers/Api/ContactController.php
+++ app/Http/Controllers/Api/ContactController.php
@@ -5,9 +5,15 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+//model
+use App\Models\Contact;
+
+use App\Http\Requests\Contact\StoreRequest as ContactStoreRequest;
+
 class ContactController extends Controller
 {
-    public function store(Request $request){
-        return response()->json($request->all());
+    public function store(ContactStoreRequest $request){
+        $contact = Contact::create();
+        return response()->json($request->validated());
     }
 }
